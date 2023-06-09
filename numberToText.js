c9b56fdddbccb28d440697290eb3ba452aa5cfae
Mathieu Giorgiutti <mathieu.giorgiutti@hubcollab.com>
2023-06-09 16:04:56 +0200

    trying to register

--- numberToText.js
+++ numberToText.js
@@ -1,37 +1,82 @@
 import {
+  css,
   html,
   LitElement,
+  styleMap,
 } from "https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js";
-export class HelloWorld extends LitElement {
-  static properties = {
-    who: { type: String },
-  };
+
+export class SampleIframe extends LitElement {
+  // Define scoped styles right with your component, in plain CSS
+  static styles = css`
+    //Add custom CSS. See https://help.nintex.com/en-US/formplugins/Reference/Style.htm
+    :host {
+      height: 100%;
+      width: 100%;
+      display: block;
+    }
+
+    .frame {
+      display: inline-block;
+      height: 100%;
+      width: 100%;
+      background-color: transparent;
+      border: none;
+    }
+  `;
 
   static getMetaConfig() {
+    // plugin contract information
     return {
-      controlName: "Hello World",
+      controlName: "IFrame-new",
       fallbackDisableSubmit: false,
-      version: "1.2",
+      description: "IFrame component which can render url view with the frame",
+      iconUrl: "one-line-text",
+      groupName: "Visual",
+      version: "1.3",
       properties: {
-        who: {
+        //Custom configuration fields. See https://help.nintex.com/en-US/formplugins/Reference/CustomField.htm
+        src: {
           type: "string",
-          title: "Who",
-          description: "Who to say hello to",
+          title: "Source URL",
+          description:
+            "URL of the iframe, please note many sites block been rendered in iframes",
         },
+        height: {
+          type: "string",
+          title: "Height",
+          description: "Height of the component",
+        },
+      },
+      standardProperties: {
+        readOnly: true, //Add a read-only mode. See https://help.nintex.com/en-US/formplugins/Reference/ReadOnly.htm
+        required: true,
+        description: true,
       },
     };
   }
 
-  constructor() {
-    super();
-    this.who = "World";
-  }
+  static properties = {
+    name: "Hello",
+    title: "Hello",
+    src: "https://www.wikipedia.org/",
+    height: "100%",
+  };
 
+  // Render the UI as a function of component state
   render() {
-    return html`<p>Hello ${this.who}</p>
-      <p />`;
+    let styles = { height: this.height };
+
+    return html` <iframe
+      class="frame"
+      style=${styleMap(styles)}
+      name=${this.name}
+      allow="geolocation *; microphone; camera"
+      title=${this.title}
+      src=${this.src}
+    ></iframe>`;
   }
 }
 
-const elementName = "hello-world";
-customElements.define(elementName, HelloWorld);
+// registering the web component.
+const elementName = "sample-iframe";
+customElements.define(elementName, SampleIframe);
